820665315c2f5a597c44f688c78e8391afd0ca3e
Linda Xiao <linda.xiao.20@dartmouth.edu>
2018-11-07 02:06:55 -0500

    Upload photo to pdf but super glitchy

--- upload.js
+++ upload.js
@@ -1,27 +1,6 @@
 // https://codepen.io/ianaya89/pen/JoRNyK/
+// https://stackoverflow.com/questions/12368910/html-display-image-after-selecting-filename
 
-// const url = 'process.php';
-// const form = document.querySelector('form');
-
-// form.addEventListener('submit', e => {
-//     e.preventDefault();
-
-//     const files = document.querySelector('[type=file]').files;
-//     const formData = new FormData();
-
-//     for (let i = 0; i < files.length; i++) {
-//         let file = files[i];
-
-//         formData.append('files[]', file);
-//     }
-
-//     fetch(url, {
-//         method: 'POST',
-//         body: formData
-//     }).then(response => {
-//         console.log(response);
-//     });
-// });
 var qrcode = new QRCode(document.getElementById("qrcode"), {
     width : 200,
     height : 200
@@ -52,6 +31,8 @@ var qrcode = new QRCode(document.getElementById("qrcode"), {
     });
 
 button = document.getElementById('btnDownload');
+// scene = document.getElementById('scene');
+var scene;
 
 function generatePDF(){
 
@@ -61,10 +42,46 @@ function generatePDF(){
 
   var doc = new jsPDF('l');
 
-  doc.setFontSize(14);
-  doc.text(20, 20, 'content.value');
-  doc.addImage(imgData, 'JPEG', 15, 15, 50, 50);
+  if (scene) {
+    // console.log(sceneData);
+    doc.addImage(scene, 'JPEG', 15, 15, 265, 150);
+    doc.addPage();
+  }
+
+  doc.addImage(imgData, 'JPEG', 20, 20, 50, 50);
   doc.save('sketchbook.pdf');
+
 }
+
+function encodeImageFileAsURL(cb) {
+    return function() {
+        var file = this.files[0];
+        var reader = new FileReader();
+        reader.onloadend = function() {
+            cb(reader.result);
+            image = document.getElementById("scene");
+            image.src = reader.result;
+            var myCanvas = convertImageToCanvas(image);
+            scene = myCanvas.toDataURL("image/jpeg");
+        }
+        reader.readAsDataURL(file);
+    }
+}
+
+// Converts image to canvas; returns new canvas element
+function convertImageToCanvas(image) {
+  var canvas = document.createElement("canvas");
+  canvas.width = image.width;
+  canvas.height = image.height;
+  canvas.getContext("2d").drawImage(image, 0, 0);
+
+  return canvas;
+}
+
+$('#inputFileToLoad').change(encodeImageFileAsURL(function(base64Img) {
+  $('#scene')
+    .find('img')
+      .attr('src', base64Img);
+}));
 
 button.addEventListener('click', generatePDF);
